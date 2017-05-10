940df84007f106b248027e04d96585843f765f04
Artik292 <artik292@gmail.com>
2017-05-10 21:22:44 +0300

    try to delete

--- friends.php
+++
@@ -1,6 +0,0 @@
-<?PHP
-
-require 'init.php';
-
-$c = new \atk4\ui\table();
-$c->setModel(new friends($db));
--- index.php
+++
@@ -1,7 +0,0 @@
-<?PHP
-require 'init.php';
-
-$grid = $layout->add('CRUD');
-$grid->setModel(new friends($db));
-
-//echo 'Hello World';
